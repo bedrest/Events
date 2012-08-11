e44d4cd18e72aaf60370caa574d82f340514de6c
Geoff Adams <geoff@dianode.net>
2012-08-11 03:24:05 +0100

    Added PHPUnit configuration and bootstrap

---
+++ tests/bootstrap.php
@@ -0,0 +1,32 @@
+<?php
+
+$paths = array(
+    realpath(__DIR__ . '/../library'),
+    realpath(__DIR__ . '/../vendor'),
+    realpath(__DIR__),
+    get_include_path()
+);
+
+set_include_path(implode(PATH_SEPARATOR, $paths));
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+/**
+ * Taken from http://zaemis.blogspot.co.uk/2012/05/writing-minimal-psr-0-autoloader.html
+ * 
+ * Using this instead of spl_autoload() as it is completely, utterly broken. It lowercases all class names, meaning
+ * that case-sensitive file systems don't have a hope of actually loading the classes you require.
+ * Even on case-insensitive file systems this is still a problem if you use the PHPUnit autoloader in combination with
+ * spl_autoload(), as PHPUnit's autoloader will include the file once using the proper file name, but the include
+ * performed by PHP_CodeCoverage will include the file again with a lowercase filename. This causes an error as it is
+ * an attempt to redeclare an existing class.
+ */
+spl_autoload_register(function ($classname) {
+    $classname = ltrim($classname, "\\");
+    preg_match('/^(.+)?([^\\\\]+)$/U', $classname, $match);
+    $classname = str_replace("\\", "/", $match[1])
+        . str_replace(array("\\", "_"), "/", $match[2])
+        . ".php";
+    include_once $classname;
+});
+
